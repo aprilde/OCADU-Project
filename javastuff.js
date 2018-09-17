c5d0dafdc2d432be4666bbcf4e88b0c11c5c5e87
April De Zen <aprildezen@Aprils-MacBook.local>
2018-09-16 22:57:15 -0400

    Clearly I'm in over my head

--- javastuff.js
+++ javastuff.js
@@ -1,38 +1,38 @@
 var canv;
+var particles;
+
 
 function setup(){
-    //createcanvas returns a reference to the canvas
-    can = createCanvas(400,400);
-    canv.mouseClicked(doSomethingIntresting);
-    particles = [],
+    // createcanvas returns a reference to the canvas
+   canv = createCanvas(400,400);
+   //canv.position(window.innerWidth/2,window.innerHeight/2);
+   //canv.mouseOver(removecanvas);
+   canv.mouseClicked(makeParticle);
+
+   particles = [];
 }
+
 function draw(){
-    //draw all our particels
-    for ( var i =0; i <particles.length; i++){
-       _ellipse (particles[i].x = particles[i].x + particles[i]speed;) 
-        //particles[i].x = particles[i].x + particles[i]speed;
-        //particles[i].y = particles[i].y + particles[i]speed;
-        //OR
-        particles[i].x =+ particles[i]speed;
-        particles[i].y =+ particles[i]speed;
-        //add the right part to the left part 
-        //works for addition
-        
+    // draw all of our particles
+    for ( var i = 0; i < particles.length; i++){
+        ellipse(particles[i].x, particles[i].y, particles[i].size);
+        //particles[i].x = particles[i].x + particles[i].speed;
+        //particles[i].y = particles[i].y + particles[i].speed;
+        // compound assignment operator
+        // add the right part to the left part
+        // += -= *= /= 
+        particles[i].x += particles[i].speed;
+        particles[i].y += particles[i].speed;
     }
-    //ellipse(300,300,40);
-    //Color(255);
-    //_doStroke();
-    
+    //ellipse(width/2, height/2, 40);
 }
-function doSomethingIntresting(){
-    
-}
-function makeParticles(){
+
+function makeParticle(){
     var particle = {
-        x:mouseX,
-        y:mouseY,
-        size: 30,
-        speed: 1,
-    }
-particles.push(particles);
+        x: mouseX,
+        y: mouseY,
+        size: random(20,40),
+        speed: random(-2, 2)
+    };
+    particles.push( particle );
 }
